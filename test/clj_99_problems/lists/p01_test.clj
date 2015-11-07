c770b458289df06b35a204605f5486ad548f65da
Rui Serra <ruipserra@gmail.com>
2015-11-07 10:57:01 +0000

    Extended last' tests to test different collection types

--- test/clj_99_problems/lists/p01_test.clj
+++ test/clj_99_problems/lists/p01_test.clj
@@ -3,11 +3,33 @@
             [clj-99-problems.lists.p01 :refer :all]))
 
 (deftest test-last'
-  (is (= (last' [])
-         nil))
+  (testing "lists"
+    (is (= (last' ())
+           nil))
 
-  (is (= (last' [0])
-         0))
+    (is (= (last' '(0))
+           0))
 
-  (is (= (last' [0 1])
-         1)))
+    (is (= (last' '(0 1))
+           1)))
+
+  (testing "vectors"
+    (is (= (last' [])
+           nil))
+
+    (is (= (last' [0])
+           0))
+
+    (is (= (last' [0 1])
+           1)))
+
+  (testing "maps"
+    (is (= (last' {})
+           nil))
+
+    (is (= (last' {:a 0})
+           [:a 0]))
+
+    (is (= (last' {:a 0 :b 1})
+           [:b 1]))))
+
